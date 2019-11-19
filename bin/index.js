5ea8d7e1930fe719dbf74a2b5e42225f620cc382
elchroy <elchroy@andela.com>
2019-11-19 14:42:22 +0100

    add styling with chalk and boxed

--- bin/index.js
+++ bin/index.js
@@ -5,7 +5,9 @@ const pdfFiller = require("pdffiller");
 const cliSelect = require("cli-select");
 const users = require("../data/index");
 const userEmails = Object.keys(users);
-
+const chalk = require("chalk");
+const boxen = require("boxen");
+const { log, error, info } = console;
 readline.emitKeypressEvents(process.stdin);
 
 const rl = readline.createInterface({
@@ -13,34 +15,70 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-console.log("Welcome to the Canadian Immigration PDF Form Filler for VanHack");
+const welcomeMsg = chalk.white.underline(
+  "Welcome to the Canadian Immigration PDF Form Filler for VanHack!"
+);
 
-rl.question("Enter email address (to search) ", email => {
+const getEmail = chalk.white.italic("Enter email address (to search): ");
+const noMatchMsg = chalk.red.italic("There's no user with given email");
+const selectedUserMsg = email =>
+  chalk.yellow.italic(`Selected user with email - ${email}`);
+const infoMsg = msg => chalk.yellow.italic(msg);
+const successMsg = msg => chalk.inverse.green.bold.italic(msg);
+
+const msg = boxen(welcomeMsg, {
+  padding: 1,
+  margin: 0,
+  backgroundColor: "#545454"
+});
+
+log(msg);
+
+rl.question(getEmail, email => {
   email = email.trim();
   if (["q", "quit"].includes(email)) {
-    console.log("Bye!");
+    log("Bye!");
     return rl.close();
   }
 
-  cliSelect(
-    {
-      values: userEmails.filter(userEmail =>
-        userEmail.toLowerCase().includes(email.toLowerCase())
-      )
-    },
-    ({ id, value }) => {
-      if (id === null && value === null) {
-        // none was selected
-      } else {
-        const user = users[value];
-        const sourcePDF = "./testpdfs/immigration_unlokced.pdf";
-        const destinationPDF = `./testpdfs/vh-immigration-${value}-cnd.pdf`;
-
-        pdfFiller.fillForm(sourcePDF, destinationPDF, user, function(err) {
-          if (err) throw err;
-          console.log("In callback (we're done).");
-        });
-      }
-    }
+  const filteredEmails = userEmails.filter(userEmail =>
+    userEmail.toLowerCase().includes(email.toLowerCase())
   );
+
+  filteredEmailsObj = {};
+  filteredEmails.forEach(e => {
+    filteredEmailsObj[e] = chalk.yellow.italic(e);
+  });
+
+  if (filteredEmails.length === 0) {
+    error(noMatchMsg);
+    return rl.close();
+  } else {
+    cliSelect(
+      {
+        values: filteredEmailsObj
+      },
+      ({ id, value }) => {
+        info(selectedUserMsg(value));
+        info(infoMsg(`Generating PDF for user with email - ${email}`));
+        if (id === null && value === null) {
+          // none was selected
+          log("none was selected");
+        } else {
+          const user = users[id];
+          const sourcePDF = "./testpdfs/immigration_unlokced.pdf";
+          const destinationPDF = `./testpdfs/vh-immigration-${id}-cnd.pdf`;
+
+          pdfFiller.fillForm(sourcePDF, destinationPDF, user, function(err) {
+            if (err) throw err;
+            info(
+              successMsg(
+                `PDF generated successfully - vh-immigration-${id}-cnd.pdf`
+              )
+            );
+          });
+        }
+      }
+    );
+  }
 });
